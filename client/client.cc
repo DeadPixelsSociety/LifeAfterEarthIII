09b33cdfde03bdb5a7c8689ee4251e926b7b546e
ahugeat <hugeat.arthur@gmail.com>
2014-10-20 21:39:01 +0200

    Skeleton of the code.

---
+++ client/client.cc
@@ -0,0 +1,7 @@
+#include <common/common.h>
+
+int main(int argc, char *argv[])
+{
+	lifeAfterEarth3::common::Common::printMessage("Client started.");
+	return EXIT_SUCCESS;
+}
---
+++ common/common.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <common/common.h>
+
+/* static */ void lifeAfterEarth3::common::Common::printMessage(std::string message)
+{
+	std::cout << message << std::endl;
+}
